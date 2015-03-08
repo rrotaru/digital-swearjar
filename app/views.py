956e0dc51f048f8937d8ae52e99e6af566586579
root <root@d1stkfactory>
2015-03-08 07:36:43 +0000

    feature(api): accepts and sends POST requests
    Updated from GET to POST. Now serves index.html as well.

--- app/views.py
+++ app/views.py
@@ -1,28 +1,51 @@
+# -*- coding: utf-8 -*-
 from __future__ import division
-from flask import render_template, redirect
+from datetime import timedelta
+from flask import Flask, Response
+from flask import make_response, request, current_app
+from flask import render_template, redirect, jsonify
+from functools import update_wrapper
 from app import app
 import requests
 import json
+import os.path
+
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
 
 #The base html page, nothing here yet
 @app.route("/")
 def hello():
-    return "Hello World!"
+    content = get_file('../index.html')
+    return Response(content, mimetype="text/html")
 
 #API Call to show balance
-@app.route("/account_balance/<path:account_number>/<path:password>")
-def show_balance(account_number, password):
-    guid = account_number 
-    main_password = password
+@app.route("/balance",methods=['POST'])
+def show_balance():
+    payload = {'password':request.form['password']}
+    guid = request.form['guid']
+    print payload, guid
 
-    baseurl = "https://blockchain.info/merchant/"+guid+"/balance?password="+main_password
-    
-    r = requests.get(baseurl)
+    baseurl = "https://blockchain.info/merchant/"+request.form['guid']+"/balance"
+    r = requests.post(baseurl,data=payload)
     jsondata = r.json()
+    print jsondata
+    return jsonify(**jsondata)
 
-    balance = str((jsondata['balance']) / 100000000)
-
-    return balance
+    #balance = str((jsondata['balance']) / 100000000)
+    #return balance
 
 #API call to send payments to other accounts
 @app.route("/payment/<path:account_number>/<path:password>/<path:address>/<path:amount>/")
