4d2ba6546c45e4b09f356f9eee11b9667212ff26
Sehmon Burnam <sehmon3@gmail.com>
2015-03-08 01:56:09 -0500

    added all API Endpoints, need to fix the new address call

--- app/views.py
+++ app/views.py
@@ -40,3 +40,39 @@ def make_payment(account_number, password, address, amount):
     message = str(jsondata['message'])
 
     return message 
+
+@app.route("/create_wallet/<path:password>/<path:label>/<path:email>")
+def create_wallet(password, label, email):
+    password = password
+    label = label
+    email = email
+
+    baseurl = "https://blockchain.info/api/v2/create_wallet?password="+password+"&label="+label+"&api_code=6c58b8d9-f429-4af0-a7ae-be98dbeb62f8&"+email
+
+    r = requests.get(baseurl)
+    try:
+        jsondata = r.json()
+    except Exception as e:
+        print e
+
+    guid = jsondata['guid']
+    link = jsondata['link']
+
+    return ("New GUID: %s @ %s" % (guid, link))
+
+@app.route("/new_address/<path:guid>/<path:password>/<path:label>")
+def new_address(guid, password, label):
+    guid = guid
+    password = password
+    label = label
+
+    baseurl = "https://blockchain.info/merchant/"+guid+"/new_address?password="+password+"&label="+label
+    """
+    r = requests.get(baseurl)
+    jsondata = r.json()
+
+    address = jsondata['address']
+    label = jsondata['label']
+    """
+
+    return ("Created new address: %s named: %s" % (address, label))
